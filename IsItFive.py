e9757a6e3198071ba1c8c506f37cb344956d707c
rjq9 <69802699+rjq9@users.noreply.github.com>
2021-09-22 18:51:08 -0400

    Update IsItFive.py

--- IsItFive.py
+++ IsItFive.py
@@ -1,2 +1,11 @@
-x = int(input())
-print(True) if x == 5 else print(False)
+def isfive(x):
+  if x == 5:
+    return True
+  else:
+    return False
+def isfiveprinter(x):
+  if x == 5:
+    print(tuple(str(True)))
+  else:
+    print(tuple(str(False)))
+    
